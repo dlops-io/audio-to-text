93c68be84b9d458fa776b707d5bab5642e06d73b
Shivas Jayaram <shivasj@gmail.com>
2021-09-15 09:17:53 -0400

    Updated to use mic-recorder-to-mp3

--- frontend/src/app/Theme.js
+++ frontend/src/app/Theme.js
@@ -7,13 +7,13 @@ const Theme = createMuiTheme({
         type: 'light',
         primary: {
             // light: will be calculated from palette.primary.main,
-            main: '#7500f7',
+            main: '#006837',
             // dark: will be calculated from palette.primary.main,
             // contrastText: will be calculated to contrast with palette.primary.main
         },
         secondary: {
             light: '#0066ff',
-            main: '#7500f7',
+            main: '#006837',
             // dark: will be calculated from palette.secondary.main,
             contrastText: '#ffffff',
         },
@@ -28,19 +28,19 @@ const Theme = createMuiTheme({
     typography: {
         useNextVariants: true,
         h6: {
-            color: "#7500f7",
+            color: "#006837",
             fontSize: "1.1rem",
             fontFamily: "Roboto, Helvetica, Arial, sans-serif",
             fontWeight: 800
         },
         h5: {
-            color: "#7500f7",
+            color: "#006837",
             fontSize: "1.2rem",
             fontFamily: "Roboto, Helvetica, Arial, sans-serif",
             fontWeight: 800
         },
         h4: {
-            color: "#7500f7",
+            color: "#006837",
             fontSize: "1.8rem",
             fontFamily: "Roboto, Helvetica, Arial, sans-serif",
             fontWeight: 900
