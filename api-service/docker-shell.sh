a3a2a8f81319fcb7e1cc95cdcc6d854f20bb5539
Shivas Jayaram <shivasj@gmail.com>
2021-09-21 12:20:18 -0400

    Updated dockerfile + added route for transcription

--- api-service/docker-shell.sh
+++ api-service/docker-shell.sh
@@ -6,6 +6,8 @@ set -e
 # Define some environment variables
 export IMAGE_NAME="audio-to-text-api-service"
 export BASE_DIR=$(pwd)
+export SECRETS_DIR=$(pwd)/../secrets/
+export GOOGLE_APPLICATION_CREDENTIALS=/secrets/gcp-service.json
 
 # Build the image based on the Dockerfile
 docker build -t $IMAGE_NAME -f Dockerfile .
@@ -13,5 +15,7 @@ docker build -t $IMAGE_NAME -f Dockerfile .
 # Run the container
 docker run --rm --name $IMAGE_NAME -ti \
 --mount type=bind,source=$BASE_DIR,target=/app \
+--mount type=bind,source="$SECRETS_DIR",target=/secrets \
 -p 9000:9000 \
+-e GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
 -e DEV=1 $IMAGE_NAME
